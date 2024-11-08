60f90569e08d084bc2e7d38865438ad207360dab
Tayomide <tibrahim@hawk.iit.edu>
2024-11-08 14:37:11 -0600

    Remove authorisation for testing

--- graphql/resolvers/subscription/floorUpdate.js
+++ graphql/resolvers/subscription/floorUpdate.js
@@ -1,20 +1,17 @@
 const pubsub = require("../../../utils/pubsub")
 const { withFilter } = require("../../../utils/withFilter")
-const { withAuthorisation } = require("../../../utils/withAuthorisation")
+// const { withAuthorisation } = require("../../../utils/withAuthorisation")
 
 // Define a filtered subscription for floor updates using 'withFilter'
 // 'withFilter' takes two arguments:
 // 1. A function that returns an async iterator for the subscription topic (in this case, "FLOOR_UPDATE")
 // 2. A function that acts as a filter to decide whether the event should be passed to the subscriber
-const floorUpdate = withAuthorisation(
-  withFilter(
-    () => pubsub.asyncIterator("FLOOR_UPDATE"),
-    async (payload, variables) => {
-      const floorUpdate = await payload.floorUpdate
-      return floorUpdate?.id === variables?.id
-    }
-  ), 
-  ["user", "admin"]
+const floorUpdate =withFilter(
+  () => pubsub.asyncIterator("FLOOR_UPDATE"),
+  async (payload, variables) => {
+    const floorUpdate = await payload.floorUpdate
+    return floorUpdate?.id === variables?.id
+  }
 )
 
 // Export the filtered subscription for use in GraphQL resolvers
