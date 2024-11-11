d30c0c58d6de70bce09a7922b809a471ede3c40b
Tayomide <tibrahim@hawk.iit.edu>
2024-11-11 14:29:02 -0600

    Catch exceptions for debugging

--- graphql/resolvers/mutation/updateFloor.js
+++ graphql/resolvers/mutation/updateFloor.js
@@ -15,62 +15,68 @@ const updateFloor = async ({
 }, context) => {
   if(!context?.isAuth) throw new Error("Error updating Floor. You are not authenticated.")
 
-  const floor = await FloorModel.findById(id)
-  const currentBuilding = await Building.build(floor.building, context)
+  try{
 
-  if(!floor) {
-    throw new Error(`Floor ${id} not found`)
-  }
-
-  if(isDeleted) {
-    await FloorModel.findOneAndDelete({_id: new ObjectId(`${id}`)})
-    updateLog("floor", floor.id, floor.name)
-    createLog("FLOOR_DELETED", `Floor ${floor.name} on ${formatModel(currentBuilding, "building", "Building")} has been deleted`,
-    {
-      buildings: [currentBuilding.id]
-    })
-    // Create logic for when a floor is deleted and when a floor name is updated
-    return null
-  }
-  const logs = [], updateLogs = []
-  if(name?.length && name !== floor.name){
-    const oldName = floor.name
-    floor.name = name
-    updateLogs.push(["floor", floor.id, oldName, floor.name])
-    logs.push(["FLOOR_NAME_CHANGE", `Floor ${oldName} on ${formatModel(currentBuilding, "building", "Building")} has been renamed to ${formatModel(floor, "floor")}`])
-  }
-  if(buildingId?.length && buildingId !== floor.building._id.toString()){
-    try{
-      const newBuilding = await Building.build(buildingId, context)
-      logs.push(["FLOOR_RELOCATED", `${formatModel(floor, "floor", "Floor")} has been moved to ${formatModel(newBuilding, "building", "Building")} from ${formatModel(currentBuilding, "building", "Building")} `, {
-        buildings: [currentBuilding.id, newBuilding.id],
+    const floor = await FloorModel.findById(id)
+    const currentBuilding = await Building.build(floor.building, context)
+  
+    if(!floor) {
+      throw new Error(`Floor ${id} not found`)
+    }
+  
+    if(isDeleted) {
+      await FloorModel.findOneAndDelete({_id: new ObjectId(`${id}`)})
+      updateLog("floor", floor.id, floor.name)
+      createLog("FLOOR_DELETED", `Floor ${floor.name} on ${formatModel(currentBuilding, "building", "Building")} has been deleted`,
+      {
+        buildings: [currentBuilding.id]
+      })
+      // Create logic for when a floor is deleted and when a floor name is updated
+      return null
+    }
+    const logs = [], updateLogs = []
+    if(name?.length && name !== floor.name){
+      const oldName = floor.name
+      floor.name = name
+      updateLogs.push(["floor", floor.id, oldName, floor.name])
+      logs.push(["FLOOR_NAME_CHANGE", `Floor ${oldName} on ${formatModel(currentBuilding, "building", "Building")} has been renamed to ${formatModel(floor, "floor")}`])
+    }
+    if(buildingId?.length && buildingId !== floor.building._id.toString()){
+      try{
+        const newBuilding = await Building.build(buildingId, context)
+        logs.push(["FLOOR_RELOCATED", `${formatModel(floor, "floor", "Floor")} has been moved to ${formatModel(newBuilding, "building", "Building")} from ${formatModel(currentBuilding, "building", "Building")} `, {
+          buildings: [currentBuilding.id, newBuilding.id],
+          floors: [floor.id]
+        }])
+      }catch{
+        console.log("Building id didn't exist so I'll wait for it to fail")
+      }
+      floor.building = buildingId
+    }
+    if(image && image !== floor.image){
+      // I haven't done image yet..., please work on that
+      // Also work on making the ()[][] a callable function for readability
+      logs.push(["FLOOR_IMAGE_UPLOADED", `A new image has been uploaded to ${formatModel(floor, "floor", "Floor")} in ${formatModel(currentBuilding, "building", "Building")}`, {
+        buildings: [currentBuilding.id],
         floors: [floor.id]
       }])
-    }catch{
-      console.log("Building id didn't exist so I'll wait for it to fail")
+      floor.image = image
     }
-    floor.building = buildingId
+  
+    await floor.save()
+    // If it saves successfully then add the logs
+    for(const [modelType, id, oldName, newName] of updateLogs){
+      updateLog(modelType, id, oldName, newName)
+    }
+    for(const [type, message, ids] of logs){
+      // I'm not adding await since I want this to be non blocking
+      createLog(type, message, ids)
+    }
+    return new Floor(floor, context)
+  }catch(e){
+    console.log(e)
+    return null
   }
-  if(image && image !== floor.image){
-    // I haven't done image yet..., please work on that
-    // Also work on making the ()[][] a callable function for readability
-    logs.push(["FLOOR_IMAGE_UPLOADED", `A new image has been uploaded to ${formatModel(floor, "floor", "Floor")} in ${formatModel(currentBuilding, "building", "Building")}`, {
-      buildings: [currentBuilding.id],
-      floors: [floor.id]
-    }])
-    floor.image = image
-  }
-
-  await floor.save()
-  // If it saves successfully then add the logs
-  for(const [modelType, id, oldName, newName] of updateLogs){
-    updateLog(modelType, id, oldName, newName)
-  }
-  for(const [type, message, ids] of logs){
-    // I'm not adding await since I want this to be non blocking
-    createLog(type, message, ids)
-  }
-  return new Floor(floor, context)
 }
 
 module.exports = {
--- graphql/resolvers/mutation/updateNode.js
+++ graphql/resolvers/mutation/updateNode.js
@@ -16,101 +16,105 @@ const updateNode = async ({
   updateNodeInput: { id, name, state, isExit, ui, isDeleted }
 }, context) => {
   if(!context?.isAuth) throw new Error("Error updating Node. You are not authenticated.")
+  try{
+    if(isDeleted){
+      const node = await NodeModel.findOneAndDelete({
+        _id: new ObjectId(`${id}`)
+      })
+      const currentFloor = await Floor.build(node.floor, context)
+      const currentBuilding = await currentFloor.building()
+      updateLog("node", node.id, node.name)
+      createLog("NODE_DELETED", `Node ${node.name} on ${formatModel(currentFloor, "floor", "Floor")} has been deleted`, {
+        floors: [currentFloor.id],
+        buildings: [currentBuilding.id]
+      })
+      return null
+    }
 
-    
-  if(isDeleted){
-    const node = await NodeModel.findOneAndDelete({
-      _id: new ObjectId(`${id}`)
-    })
+    if(!node){
+      throw new Error("Node not found")
+    }
+
+    const node = await NodeModel.findById(id)
     const currentFloor = await Floor.build(node.floor, context)
     const currentBuilding = await currentFloor.building()
-    updateLog("node", node.id, node.name)
-    createLog("NODE_DELETED", `Node ${node.name} on ${formatModel(currentFloor, "floor", "Floor")} has been deleted`, {
-      floors: [currentFloor.id],
-      buildings: [currentBuilding.id]
-    })
+    const logs = [], updateLogs = []
+
+    if(name?.length && name !== node.name) {
+      const oldName = node.name
+      node.name = name
+      updateLogs.push(["node", node.id, oldName, node.name])
+      logs.push(["NODE_NAME_CHANGE", `Node ${oldName} on ${formatModel(currentFloor, "floor", "Floor")} has been renamed to ${formatModel(node, "node")}`, {
+        buildings: [currentBuilding.id],
+        floors: [currentFloor.id],
+        nodes: [node.id]
+      }])
+    }
+    if(state?.length && state !== node.state){
+      switch(state){
+        case "safe":
+          logs.push(["NODE_SAFE", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} is now safe`, {
+            buildings: [currentBuilding.id],
+            floors: [currentFloor.id],
+            nodes: [node.id]
+          }])
+          break
+        case "stuck":
+          logs.push(["NODE_STUCK", `There is currently no way out from ${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")}`, {
+            buildings: [currentBuilding.id],
+            floors: [currentFloor.id],
+            nodes: [node.id]
+          }])
+          break
+        case "compromised":
+          logs.push(["NODE_COMPROMISED", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} has detected fire!`, {
+            buildings: [currentBuilding.id],
+            floors: [currentFloor.id],
+            nodes: [node.id]
+          }])
+          break
+        default:
+          logs.push(["NODE_STATE_CHANGE", `${formatModel(node, "node", "Node")} state has been changed on ${formatModel(currentFloor, "floor", "Floor")}`, {
+            buildings: [currentBuilding.id],
+            floors: [currentFloor.id],
+            nodes: [node.id]
+          }])
+          break
+      }
+      node.state = state
+      // Keep different states for on fire, stuck and stuff like that
+    }
+    if(typeof isExit === "boolean" && isExit != node.isExit){
+      logs.push(["NODE_EXIT", `${formatModel(node, "node", "Node")} has been assigned as an exit on ${formatModel(currentFloor, "floor", "Floor")}`, {
+        buildings: [currentBuilding.id],
+        floors: [currentFloor.id],
+        nodes: [node.id]
+      }])
+      node.isExit = isExit
+    }
+    if(ui && (ui?.x !== node.ui.x || ui?.y !== node.ui.y)){
+      logs.push(["NODE_LOCATION_CHANGED", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} has been moved`, {
+        buildings: [currentBuilding.id],
+        floors: [currentFloor.id],
+        nodes: [node.id]
+      }])
+      node.ui = ui
+    }
+    await node.save()
+
+    for(const [modelType, id, oldName, newName] of updateLogs){
+      updateLog(modelType, id, oldName, newName)
+    }
+    for(const [type, message, ids] of logs){
+      createLog(type, message, ids)
+    }
+
+    return new Node(node, context)
+  } catch(err){
+    console.log(err)
     return null
   }
-  
-  if(!node){
-    throw new Error("Node not found")
-  }
-  
-  const node = await NodeModel.findById(id)
-  const currentFloor = await Floor.build(node.floor, context)
-  const currentBuilding = await currentFloor.building()
-  const logs = [], updateLogs = []
-
-  if(name?.length && name !== node.name) {
-    const oldName = node.name
-    node.name = name
-    updateLogs.push(["node", node.id, oldName, node.name])
-    logs.push(["NODE_NAME_CHANGE", `Node ${oldName} on ${formatModel(currentFloor, "floor", "Floor")} has been renamed to ${formatModel(node, "node")}`, {
-      buildings: [currentBuilding.id],
-      floors: [currentFloor.id],
-      nodes: [node.id]
-    }])
-  }
-  if(state?.length && state !== node.state){
-    switch(state){
-      case "safe":
-        logs.push(["NODE_SAFE", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} is now safe`, {
-          buildings: [currentBuilding.id],
-          floors: [currentFloor.id],
-          nodes: [node.id]
-        }])
-        break
-      case "stuck":
-        logs.push(["NODE_STUCK", `There is currently no way out from ${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")}`, {
-          buildings: [currentBuilding.id],
-          floors: [currentFloor.id],
-          nodes: [node.id]
-        }])
-        break
-      case "compromised":
-        logs.push(["NODE_COMPROMISED", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} has detected fire!`, {
-          buildings: [currentBuilding.id],
-          floors: [currentFloor.id],
-          nodes: [node.id]
-        }])
-        break
-      default:
-        logs.push(["NODE_STATE_CHANGE", `${formatModel(node, "node", "Node")} state has been changed on ${formatModel(currentFloor, "floor", "Floor")}`, {
-          buildings: [currentBuilding.id],
-          floors: [currentFloor.id],
-          nodes: [node.id]
-        }])
-        break
-    }
-    node.state = state
-    // Keep different states for on fire, stuck and stuff like that
-  }
-  if(typeof isExit === "boolean" && isExit != node.isExit){
-    logs.push(["NODE_EXIT", `${formatModel(node, "node", "Node")} has been assigned as an exit on ${formatModel(currentFloor, "floor", "Floor")}`, {
-      buildings: [currentBuilding.id],
-      floors: [currentFloor.id],
-      nodes: [node.id]
-    }])
-    node.isExit = isExit
-  }
-  if(ui && (ui?.x !== node.ui.x || ui?.y !== node.ui.y)){
-    logs.push(["NODE_LOCATION_CHANGED", `${formatModel(node, "node", "Node")} on ${formatModel(currentFloor, "floor", "Floor")} has been moved`, {
-      buildings: [currentBuilding.id],
-      floors: [currentFloor.id],
-      nodes: [node.id]
-    }])
-    node.ui = ui
-  }
-  await node.save()
-
-  for(const [modelType, id, oldName, newName] of updateLogs){
-    updateLog(modelType, id, oldName, newName)
-  }
-  for(const [type, message, ids] of logs){
-    createLog(type, message, ids)
-  }
-  
-  return new Node(node, context)
+    
 }
 
 module.exports = {
